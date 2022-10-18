651f4be8a769aced7442d0592bef96975d439ff4
Luom Pham <luompham95@gmail.com>
2022-10-18 18:01:35 +0700

    first commit

---
+++ src/route/web.js
@@ -0,0 +1,20 @@
+import express from "express";
+import homeController from "../controllers/homeController";
+
+let router = express.Router();
+
+let initWebRoutes = (app) => {
+    router.get('/', homeController.getHomePage);
+
+    router.get('/about', homeController.getAboutPage);
+
+    router.get('/hoidanit', (req, res) => {
+        return res.send('Hello world with hoi dan it')
+    });
+
+    //rest api
+    return app.use("/", router);
+}
+
+module.exports = initWebRoutes;
+
